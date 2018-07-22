2a7bb4ef886b40cf88b91bd78e3b3bf3ce1d356a
arvida.jimenez@gmail.com <arvida.jimenez@gmail.com>
2018-07-22 00:46:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-    puts
-    "    |   |
-      -----------
-         |   |
-      -----------
-         |   |   "
+    a = ["   |", "   ", "|   "]
+    b = [-----------]
 	end
